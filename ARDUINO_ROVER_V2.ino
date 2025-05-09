ffd82cab9a49f7d6be6318c60c4c0f9395656791
Sweing Nakarmi <sweing111@gmail.com>
2025-05-09 18:43:39 +0545

    merged logic for bluetooth and voice control

--- ARDUINO_ROVER_V2.ino
+++ ARDUINO_ROVER_V2.ino
@@ -1,6 +1,3 @@
-/*obstacle avoiding, Bluetooth control, voice control robot car.
-   Home Page
-*/
 #include <Servo.h>
 #include <AFMotor.h>
 #define Echo A0
@@ -8,19 +5,15 @@
 #define motor 10
 #define Speed 150
 #define spoint 103
+
 char value;
 int distance;
-int Left;
-int Right;
-int L = 0;
-int R = 0;
-int L1 = 0;
-int R1 = 0;
 Servo servo;
 AF_DCMotor M1(1);
 AF_DCMotor M2(2);
 AF_DCMotor M3(3);
 AF_DCMotor M4(4);
+
 void setup() {
   Serial.begin(9600);
   pinMode(Trig, OUTPUT);
@@ -31,89 +24,64 @@ void setup() {
   M3.setSpeed(Speed);
   M4.setSpeed(Speed);
 }
+
 void loop() {
-  // Obstacle();
-  // Bluetoothcontrol();
-  voicecontrol();
+  // Obstacle(); // Uncomment if needed
+  control();
 }
-void Bluetoothcontrol() {
+
+void control() {
   if (Serial.available() > 0) {
     value = Serial.read();
     Serial.println(value);
-  }
-  if (value == 'F') {
-    forward();
-  } else if (value == 'B') {
-    backward();
-  } else if (value == 'L') {
-    left();
-  } else if (value == 'R') {
-    right();
-  } else if (value == 'S') {
-    Stop();
-  }
-}
-void Obstacle() {
-  distance = ultrasonic();
-  if (distance <= 12) {
-    Stop();
-    backward();
-    delay(100);
-    Stop();
-    L = leftsee();
-    servo.write(spoint);
-    delay(800);
-    R = rightsee();
-    servo.write(spoint);
-    if (L < R) {
-      right();
-      delay(500);
-      Stop();
-      delay(200);
-    } else if (L > R) {
-      left();
-      delay(500);
-      Stop();
-      delay(200);
-    }
-  } else {
-    forward();
-  }
-}
-void voicecontrol() {
-  if (Serial.available() > 0) {
-    value = Serial.read();
-    Serial.println(value);
-    if (value == '^') {
+
+    // Handle both Bluetooth and Voice commands
+    if (value == 'F') { // Bluetooth forward
       forward();
-    } else if (value == '-') {
+    } else if (value == 'B') { // Bluetooth backward
       backward();
-    } else if (value == '<') {
-      L = leftsee();
+    } else if (value == 'L') { // Bluetooth left
+      left();
+    } else if (value == 'R') { // Bluetooth right
+      right();
+    } else if (value == 'S') { // Bluetooth stop
+      Stop();
+    } else if (value == '^') { // Voice forward (timed)
+      forward();
+      delay(2000);  // Move for 2 seconds
+      Stop();
+    } else if (value == '-') { // Voice backward (timed)
+      backward();
+      delay(2000);  // Move for 2 seconds
+      Stop();
+    } else if (value == '<') { // Voice left
+      int leftDistance = leftsee();
       servo.write(spoint);
-      if (L >= 10 ) {
+      delay(800);
+      if (leftDistance >= 10) {
         left();
         delay(500);
         Stop();
-      } else if (L < 10) {
+      } else {
         Stop();
       }
-    } else if (value == '>') {
-      R = rightsee();
+    } else if (value == '>') { // Voice right
+      int rightDistance = rightsee();
       servo.write(spoint);
-      if (R >= 10 ) {
+      delay(800);
+      if (rightDistance >= 10) {
         right();
         delay(500);
         Stop();
-      } else if (R < 10) {
+      } else {
         Stop();
       }
-    } else if (value == '*') {
+    } else if (value == '*') { // Voice stop
       Stop();
     }
   }
 }
-// Ultrasonic sensor distance reading function
+
 int ultrasonic() {
   digitalWrite(Trig, LOW);
   delayMicroseconds(4);
@@ -121,48 +89,52 @@ int ultrasonic() {
   delayMicroseconds(10);
   digitalWrite(Trig, LOW);
   long t = pulseIn(Echo, HIGH);
-  long cm = t / 29 / 2; //time convert distance
-  return cm;
+  return t / 29 / 2;
 }
+
+int rightsee() {
+  servo.write(20);
+  delay(800);
+  return ultrasonic();
+}
+
+int leftsee() {
+  servo.write(180);
+  delay(800);
+  return ultrasonic();
+}
+
 void forward() {
   M1.run(FORWARD);
   M2.run(FORWARD);
   M3.run(FORWARD);
   M4.run(FORWARD);
 }
+
 void backward() {
   M1.run(BACKWARD);
   M2.run(BACKWARD);
   M3.run(BACKWARD);
   M4.run(BACKWARD);
 }
+
 void right() {
   M1.run(FORWARD);
   M2.run(FORWARD);
   M3.run(BACKWARD);
   M4.run(BACKWARD);
 }
+
 void left() {
   M1.run(BACKWARD);
   M2.run(BACKWARD);
   M3.run(FORWARD);
   M4.run(FORWARD);
 }
+
 void Stop() {
   M1.run(RELEASE);
   M2.run(RELEASE);
   M3.run(RELEASE);
   M4.run(RELEASE);
-}
-int rightsee() {
-  servo.write(20);
-  delay(800);
-  Left = ultrasonic();
-  return Left;
-}
-int leftsee() {
-  servo.write(180);
-  delay(800);
-  Right = ultrasonic();
-  return Right;
 }
